eaa8183515cd0007f9132a85991128e9da79a7c4
Miriam Völkl <miriamvoelkl@gmail.com>
2023-12-22 15:15:58 +0100

    add sum component

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={3} valueB={5} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {valueA + valueB}
+    </h1>
+  );
+  //const sum = valueA + valueB * 1;
+  //return <h1>{sum}</h1>;
 }
